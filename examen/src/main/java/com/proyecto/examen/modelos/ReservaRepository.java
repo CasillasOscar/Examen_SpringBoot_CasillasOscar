dea3aced0bc1faa270d12d2373a9b6180419de0b
O5CVR <oscarcasillasrodriguez@gmail.com>
2025-02-18 15:40:32 +0100

    Se cambia paramentro de entrada de la consulta y consulta por habitacion completa

--- examen/src/main/java/com/proyecto/examen/modelos/ReservaRepository.java
+++ examen/src/main/java/com/proyecto/examen/modelos/ReservaRepository.java
@@ -1,6 +1,7 @@
 package com.proyecto.examen.modelos;
 
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +15,6 @@ public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
     Optional<Reserva> getReservaById(Integer id);
 
     //Falla porque no puede crear la consulta, el problema que da con el id porque estan todos los bean correctos
-    Optional<Reserva> getReservaById_HabitacionEqualsAndFechaCheckoutIsBeforeAndBorradoNull(String idHabitacion, LocalDateTime fechaInicio);
+    Optional<Reserva> getReservaByIdHabitacionEqualsAndFechaCheckoutIsBeforeAndBorradoNull(@NotNull Habitacione idHabitacion, @NotNull LocalDateTime fechaCheckout);
+
 }
--- examen/src/main/java/com/proyecto/examen/services/ReservaService.java
+++ examen/src/main/java/com/proyecto/examen/services/ReservaService.java
@@ -34,7 +34,7 @@ public class ReservaService {
 
                 if(usuarioExist.isPresent()){
                     //Comprobamos si hay una reserva con checkout antes al checkin que queremos
-                    Optional<Reserva> reservaExist = reservaRepository.getReservaById_HabitacionEqualsAndFechaCheckoutIsBeforeAndBorradoNull(reserva.getIdHabitacion(),reserva.getFecha_inicio());
+                    Optional<Reserva> reservaExist = reservaRepository.getReservaByIdHabitacionEqualsAndFechaCheckoutIsBeforeAndBorradoNull(habitacionExist.get(),reserva.getFecha_inicio());
 
                     if(reservaExist.isPresent()){
 
